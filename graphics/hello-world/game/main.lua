c588d3d0b81a809f18fb7a81f5ae8bd093dbd62f
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2018-08-29 12:53:35 -0400

    Update main.lua

--- graphics/hello-world/game/main.lua
+++ graphics/hello-world/game/main.lua
@@ -1,5 +1,9 @@
 function love.load()
-
+    local down, OS = "plus", {love.system.getOS()}
+    if OS[2] == "3DS" then
+        down = "start"
+    end
+    exitKey = down
 end
 
 function love.update(dt)
@@ -7,11 +11,11 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.print("Hello World!", 300, 300)
+    love.graphics.print("Hello World!", 10, 10)
 end
 
 function love.gamepadpressed(joy, button)
-    if button == "plus" then
+    if button == exitKey then
         love.event.quit()
     end
 end
